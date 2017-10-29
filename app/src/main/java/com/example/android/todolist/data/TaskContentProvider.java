8f85e93f666b4dd82f6467c7d0f8bf0b8a3d1372
Ivan Mitkov <ivan.mitkov@gmail.com>
2017-10-29 08:20:34 +0200

    Add UriMatcher to content provider

--- app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -5,13 +5,36 @@ package com.example.android.todolist.data;
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 
+
 // completed (1) Verify that TaskContentProvider extends from ContentProvider and implements required methods
 public class TaskContentProvider extends ContentProvider {
+    // Define final integer constants for the directory of tasks and a single item.
+    // It's convention to use 100, 200, 300, etc for directories,
+    // and related ints (101, 102, ..) for items in that directory.
+    private static final int TASKS=100;
+    private static final int TASKS_WITH_ID=101;
 
+    //declare static UriMatcher for accessing in provider code
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+
+
+    //this function tell wich uri we need the matcher to recognize
+    // Define a static buildUriMatcher method that associates URI's with their int match
+    public static UriMatcher buildUriMatcher (){
+
+        UriMatcher uriMatcher=new UriMatcher(UriMatcher.NO_MATCH);
+        //directory
+        uriMatcher.addURI(TaskContract.AUTHORITY,TaskContract.PATH_TASK,TASKS);
+        //single task
+        uriMatcher.addURI(TaskContract.AUTHORITY,TaskContract.PATH_TASK+"/#",TASKS_WITH_ID);
+        return uriMatcher;
+
+    }
 
     /* onCreate() is where you should initialize anything you’ll need to setup
     your underlying data source.
