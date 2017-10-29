c68b93c69d8b77d400059a862c2d57561cc86a7d
Ivan Mitkov <ivan.mitkov@gmail.com>
2017-10-29 12:51:56 +0200

    adding uodate function to content provider

--- app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -185,12 +185,37 @@ public class TaskContentProvider extends ContentProvider {
         return row;
     }
 
-
+    // Update won't be used in the final ToDoList app but is implemented here for completeness
+    // This updates a single item (by it's ID) in the tasks directory
     @Override
     public int update(@NonNull Uri uri, ContentValues values, String selection,
                       String[] selectionArgs) {
 
-        throw new UnsupportedOperationException("Not yet implemented");
+        //Keep track of if an update occurs
+        int tasksUpdated;
+
+        // match code
+        int match = sUriMatcher.match(uri);
+
+        switch (match) {
+            case TASKS_WITH_ID:
+                //update a single task by getting the id
+                String id = uri.getPathSegments().get(1);
+                //using selections
+                tasksUpdated = mDBHelper.getWritableDatabase()
+                        .update(TABLE_NAME, values, "_id=?", new String[]{id});
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        if (tasksUpdated != 0) {
+            //set notifications if a task was updated
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        // return number of tasks updated
+        return tasksUpdated;
     }
 
 
